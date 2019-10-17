78246a65c5650ccb202f4bd2ab9b4faa59955bd9
WBSaston <afrancis46@outlook.com>
2019-10-17 14:28:35 +0100

    Testing

--- www/js/index.js
+++ www/js/index.js
@@ -8,6 +8,7 @@ function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
 	$("#paused").text("Application resumed: " + resumed_count);
+	$("#storageCount").text("The value of the key is " + value);
 }
 
 
